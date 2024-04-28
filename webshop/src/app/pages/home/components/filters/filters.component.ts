fde2701f8b141ad7e7993343a7ace5513c13f175
serhasHacizade <eren.felek.4@gmail.com>
2024-04-28 12:35:27 +0300

    commit

--- webshop/src/app/pages/home/components/filters/filters.component.ts
+++ webshop/src/app/pages/home/components/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './filters.component.scss'
 })
 export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
   categories = ["shoes", "sports"];
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+  };
 };
--- webshop/src/app/pages/home/home.component.ts
+++ webshop/src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +11,14 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+  };
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   };
 };
